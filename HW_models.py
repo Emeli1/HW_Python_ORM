45e19545a722a1dee6dfa204db2c8223b0235637
Irina Emelianova <irina.m.emelianova@gmail.com>
2025-03-05 16:49:00 +0300

    Change

--- HW_models.py
+++ HW_models.py
@@ -7,8 +7,8 @@ Base = declarative_base()
 class Publisher(Base):
     __tablename__ = 'publisher'
 
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
+    name = sq.Column(sq.String(length=40), unique=True, nullable=False)
 
     def __str__(self):
         return f'Publisher{self.id}: {self.name}'
@@ -16,7 +16,7 @@ class Publisher(Base):
 class Book(Base):
     __tablename__ = 'book'
 
-    id = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
     title = sq.Column(sq.Text, nullable=False)
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
 
@@ -28,7 +28,7 @@ class Book(Base):
 class Shop(Base):
     __tablename__ = 'shop'
 
-    id = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
     name = sq.Column(sq.String(length=40), unique=True)
 
     def __str__(self):
@@ -38,7 +38,7 @@ class Shop(Base):
 class Stock(Base):
     __tablename__ = 'stock'
 
-    id = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
     id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
@@ -53,7 +53,7 @@ class Stock(Base):
 class Sale(Base):
     __tablename__ = 'sale'
 
-    id = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
     price = sq.Column(sq.DECIMAL(10,2), nullable=False)
     date_sale = sq.Column(sq.DateTime, nullable=False)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
